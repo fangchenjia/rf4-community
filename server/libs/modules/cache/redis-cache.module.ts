14a239578b2bb6a3294830108bd566557ec41aa1
fangchenjiagy <fang_chj@hisuntech.com>
2023-08-04 17:01:24 +0800

    fix: 修复redis报错

--- server/libs/modules/cache/redis-cache.module.ts
+++ server/libs/modules/cache/redis-cache.module.ts
@@ -11,13 +11,15 @@ import { RedisCacheService } from './redis-cache.service';
       isGlobal: true,
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory(configService: ConfigService) {
-        return {
-          store: redisStore as any,
+      useFactory: async (configService: ConfigService) => {
+        const store = await redisStore({
           host: configService.get('REDIS_HOST'),
           port: configService.get('REDIS_PORT'),
           db: configService.get('REDIS_DB'), //目标库,
           password: configService.get('REDIS_PASSWORD'), // 密码,没有可以不写
+        } as any);
+        return {
+          store: store as any,
         };
       },
     }),
