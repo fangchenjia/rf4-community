91447aa0738358215aaa3a509e1dd77d845d816e
fangchenjiagy <fang_chj@hisuntech.com>
2023-10-23 20:03:21 +0800

    fix: 覆盖PassportStrategy参数校验失败默认返回401

--- server/libs/modules/auth/strategys/local.strategy.ts
+++ server/libs/modules/auth/strategys/local.strategy.ts
@@ -2,6 +2,8 @@ import { Strategy, IStrategyOptions } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
 import { AuthService } from '../auth.service';
 import { Inject } from '@nestjs/common';
+import { ApiException } from 'shared/exceptions/api.exception';
+import { ErrorEnum } from 'shared/contants/error-code.contants';
 
 export class LocalStrategy extends PassportStrategy(Strategy, 'USER_LOGIN') {
   constructor(@Inject(AuthService) private authService: AuthService) {
@@ -14,5 +16,14 @@ export class LocalStrategy extends PassportStrategy(Strategy, 'USER_LOGIN') {
   async validate(mobile: string, password: string) {
     const user = await this.authService.validateUser(mobile, password);
     return user;
+  }
+
+  authenticate(req, options) {
+    const { mobile, password } = req.body;
+    // 覆盖PassportStrategy 默认的 401 错误
+    if (!mobile || !password) {
+      throw new ApiException(ErrorEnum.INVALID_INPUT);
+    }
+    return super.authenticate(req, options);
   }
 }
