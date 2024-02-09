7c349605b7d1ba7381c5bb086c5cd2d4eb1fbce6
方陈家 <fang_chj@hisuntech.com>
2024-02-09 23:43:37 +0800

    fix: 打包后css引入顺序问题

--- web/src/project/mobile/main.ts
+++ web/src/project/mobile/main.ts
@@ -1,3 +1,11 @@
+import { createApp } from 'vue'
+import requestSetup from '@/project/mobile/request'
+import { setupStore } from '@/store'
+
+import router from './router'
+
+import App from './App.vue'
+
 import 'normalize.css'
 import './assets/main.css'
 // 包括 Toast，Dialog，Notify 和 ImagePreview 组件。在使用函数组件时，unplugin-vue-components 无法解析自动注册组件，导致 @vant/auto-import-resolver 无法解析样式，因此需要手动引入样式
@@ -6,14 +14,6 @@ import 'vant/es/dialog/style';
 import 'vant/es/notify/style';
 import 'vant/es/image-preview/style';
 import 'amfe-flexible';
-
-import { createApp } from 'vue'
-import requestSetup from '@/project/mobile/request'
-import { setupStore } from '@/store'
-
-import router from './router'
-
-import App from './App.vue'
 
 const app = createApp(App)
 
